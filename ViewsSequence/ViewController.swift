63420b57d5bab724710938531f9180c965a56f2b
Iurie Guzun <iurie_guzun@yahoo.com>
2021-03-10 12:08:36 -0500

    Show sequence

--- ViewsSequence/ViewController.swift
+++ ViewsSequence/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+    class ViewController: UIViewController {
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            print("viewDidLoad() called")
+            view.backgroundColor = randomColor()
+            sleep(2)
+            
+        }
+        
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            print("viewWillAppear(_:) called")
+              view.backgroundColor = randomColor()
+            sleep(2)
+        }
+        
+        override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            print("viewDidAppear(_:) called")
+           view.backgroundColor = randomColor()
+            sleep(2)
+        }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+  func randomColor () -> UIColor {
+    
+    let myRed = CGFloat.random(in: 0...1)
+    let myGreen = CGFloat.random(in: 0...1)
+    let myBlue = CGFloat.random(in: 0...1)
+    let randomColor = UIColor(displayP3Red: myRed, green: myGreen, blue: myBlue, alpha: 1)
+     print(randomColor)
+    return randomColor
+   
+      }
 
-
+    
 }
-
+//extension UIColor {
+//    public class var random: UIColor {
+//
+//    let myRed = CGFloat.random(in: 0...1)
+//    let myGreen = CGFloat.random(in: 0...1)
+//    let myBlue = CGFloat.random(in: 0...1)
+//    let randomColor = UIColor(red: myRed),
+//    green: green),
+//    blue: blue),
+//    alpha: 1.0)
+//    return
+//      }
+//    }
