3f2f17874aec143e820094e243cbf94f12c7da6c
Iurie Guzun <iurie_guzun@yahoo.com>
2021-03-10 11:33:10 -0500

    Initial Commit

---
+++ ViewsSequence/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewsSequence
+//
+//  Created by Iurie Guzun on 2021-03-10.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
